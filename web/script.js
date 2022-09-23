020bd0d108e93197cb05f7f07f58b43cc02b5149
Julian Descottes <jdescottes@mozilla.com>
2022-09-23 17:16:47 +0200

    Add linters

--- web/script.js
+++ web/script.js
@@ -9,13 +9,13 @@ document.querySelector("#connect").addEventListener("click", () => {
   socket.onopen = function (event) {
     document.querySelector("#connect").setAttribute("disabled", "true");
     document.querySelector("#host").setAttribute("disabled", "true");
-    document.querySelector("#connect").innerText = "connected";    
+    document.querySelector("#connect").innerText = "connected";
     addTraffic("ws", "WebSocket open");
-  };  
+  };
   socket.onclose = function (event) {
     document.querySelector("#connect").removeAttribute("disabled");
     document.querySelector("#host").removeAttribute("disabled");
-    document.querySelector("#connect").innerText = "connect";    
+    document.querySelector("#connect").innerText = "connect";
     addTraffic("ws", "WebSocket closed");
   };
 });
@@ -34,7 +34,7 @@ document.querySelector("#add-command-button").addEventListener("click", () => {
   commandEl.dataset.command = "CUSTOM" + customCommandId;
   customCommandId++;
   commandEl.innerHTML = commandMarkup;
-  
+
   document.querySelector("#commands-container").appendChild(commandEl);
 });
 
@@ -42,64 +42,66 @@ let reqId = 1;
 
 window.sendCommand = async function (method, params) {
   const id = await sendMessage({ method, params });
-  return new Promise(resolve => {
-    const removeListener = addWSMessageListener(data => { 
+  return new Promise((resolve) => {
+    const removeListener = addWSMessageListener((data) => {
       if (data.id === id) {
         removeListener();
         resolve(data);
       }
-    })
+    });
   });
-}
+};
 
 const wsMessageListeners = new Set();
 const addWSMessageListener = function (cb) {
   wsMessageListeners.add(cb);
   return () => wsMessageListeners.delete(cb);
-}
+};
 
-document.querySelector("#commands-container").addEventListener("click", async e => {
-  if (!e.target.classList.contains("command-send")) {
-    return;  
-  }
-  
-  const commandEl = e.target.closest(".command");
-  if (!commandEl) {
+document
+  .querySelector("#commands-container")
+  .addEventListener("click", async (e) => {
+    if (!e.target.classList.contains("command-send")) {
+      return;
+    }
+
+    const commandEl = e.target.closest(".command");
+    if (!commandEl) {
+      return;
+    }
+
+    const params = commandEl.querySelector("input[name=params]").value || "{}";
+    if (commandEl.dataset.command.startsWith("CUSTOM")) {
+      await sendMessage({
+        method: commandEl.querySelector("input[name=method]").value,
+        params: eval(`(${params})`),
+      });
+    } else {
+      await sendMessage({
+        method: commandEl.dataset.command,
+        params: eval(`(${params})`),
+      });
+    }
+  });
+document.querySelector("#requests-log").addEventListener("click", async (e) => {
+  if (!e.target.classList.contains("log-id")) {
     return;
   }
 
-  const params = commandEl.querySelector("input[name=params]").value || "{}";
-  if (commandEl.dataset.command.startsWith("CUSTOM")) {
-    await sendMessage({
-      "method": commandEl.querySelector("input[name=method]").value,
-      "params": eval(`(${params})`),
-    });
-  } else {
-    await sendMessage({
-      "method": commandEl.dataset.command,
-      "params": eval(`(${params})`),
-    });
-  }
-})
-document.querySelector("#requests-log").addEventListener("click", async e => {
-  if (!e.target.classList.contains("log-id")) {
-    return;  
-  }
-  
   const requestEl = e.target.closest(".request");
   if (!requestEl) {
     return;
   }
-  
+
   requestEl.classList.toggle("expanded");
-})
+});
 
 function onWebSocketMessage(event) {
   console.log(event.data);
   const data = JSON.parse(event.data);
   if (data.id) {
     addTraffic("response", data);
-    wsMessageListeners.forEach(cb => cb(data));
+    wsMessageListeners.forEach((cb) => cb(data));
   } else {
     addTraffic("event", data);
     if (isLogEvent(data)) {
@@ -131,7 +133,7 @@ function eventTemplate(type, id, text, time) {
   </div>`;
 }
 
-const nodeBuilder = document.createElement("div")
+const nodeBuilder = document.createElement("div");
 
 let fakeRequest = document.querySelector("#fake-request");
 let fakeEvent = document.querySelector("#fake-event");
@@ -139,7 +141,11 @@ let fakeEvent = document.querySelector("#fake-event");
 function addTraffic(id, msg) {
   document.querySelector("#fake-request")?.remove();
   nodeBuilder.innerHTML = logTemplate("request", id);
-  nodeBuilder.querySelector(".log-text").textContent = JSON.stringify(msg, null, "  ");
+  nodeBuilder.querySelector(".log-text").textContent = JSON.stringify(
+    msg,
+    null,
+    "  "
+  );
   const container = document.querySelector("#requests-log");
   if (!container.firstChild) {
     container.appendChild(nodeBuilder.firstChild);
@@ -156,7 +162,7 @@ function isLogEvent(data) {
 function addLogEvent(data) {
   document.querySelector("#fake-event")?.remove();
   const evt = data.params;
-  const time = new Date(evt.timestamp).toTimeString().substring(0, 8)
+  const time = new Date(evt.timestamp).toTimeString().substring(0, 8);
 
   nodeBuilder.innerHTML = eventTemplate("event", evt.method, evt.text, time);
   const container = document.querySelector("#events-log");
@@ -179,15 +185,17 @@ function clearEvents() {
   container.appendChild(fakeEvent);
 }
 
-[...document.querySelectorAll(".clear-button")].forEach(btn => btn.addEventListener("click", e => {
-  const container = e.target.dataset.container;
-  if (container === "requests") {
-    clearRequests();
-  }  
-  if (container === "events") {
-    clearEvents();
-  }
-}));
+[...document.querySelectorAll(".clear-button")].forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    const container = e.target.dataset.container;
+    if (container === "requests") {
+      clearRequests();
+    }
+    if (container === "events") {
+      clearEvents();
+    }
+  })
+);
 
 const helpArgumentsEl = document.getElementById("connection-help-arguments");
 helpArgumentsEl.textContent = helpArgumentsEl.textContent.replace(
