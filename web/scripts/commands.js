a48266f45cb982836482fa2b8ca6570f3657d776
Julian Descottes <jdescottes@mozilla.com>
2022-09-27 10:28:52 +0200

    Add evaluate and callFunction

--- web/scripts/commands.js
+++ web/scripts/commands.js
@@ -31,4 +31,12 @@ export const COMMANDS = {
     method: "browsingContext.navigate",
     placeholder: "{ context: id, url: \"http://some.url\" }",
   },
+  "script.evaluate": {
+    method: "script.evaluate",
+    placeholder: "{ target: { context: id }, awaitPromise: false, expression: \"console.log(1)\" }",
+  },
+  "script.callFunction": {
+    method: "script.callFunction",
+    placeholder: "{ target: { context: id }, awaitPromise: false, arguments: [{ type: \"number\", value: 1 }], functionDeclaration: \"(arg) => console.log(arg)\" }",
+  },
 };
